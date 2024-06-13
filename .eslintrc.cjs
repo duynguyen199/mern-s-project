30279841dfe86bc7deca0b86c22a94b0fbe6c58a
duynguyen <dannynguyen748@gmail.com>
2024-06-13 10:35:06 -0400

    updated 10:35

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,37 @@
+// Updated by trungquandev.com's author on May 13 2023
+// Sample Eslint config for Node.js project
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  env: { browser: true, es2020: true, node: true },
+  extends: [ 'eslint:recommended' ],
+  parser: '@babel/eslint-parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', allowImportExportEverywhere: true },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh'
+  ],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'react-refresh/only-export-components':'warn',
+    'react-hooks/rules-of-hooks':'error',
+    'react-hooks/exhaustive-deps':'warn',
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    'indent': ['warn', 2],
+    'semi': [1, 'never'],
+    'quotes': ['error', 'single'],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-dangle': 1,
+    'comma-spacing': 1,
+    'arrow-spacing': 1
+  }
 }
