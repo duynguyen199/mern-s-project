e220b5f46e796342a601d7f5b7202687144ab6f9
duynguyen <dannynguyen748@gmail.com>
2024-06-13 16:54:12 -0400

    update 4:54

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { CssBaseline } from '@mui/material'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './theme';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
-
-
-     <CssBaseline/>
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    ,
   </React.StrictMode>,
-)
+);
