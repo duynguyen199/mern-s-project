380414f0b2a073eaa2111bf09238cd7ccc0c19f3
duynguyen <dannynguyen748@gmail.com>
2024-07-06 13:23:46 -0400

    update 66 123

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,20 +1,91 @@
 import React from "react";
 import DarkLightMode from "../ModeSelect";
-import { Box, Container } from "@mui/material";
+import { ReactComponent as TrelloIcon } from "~/assets/trello.svg";
+import {
+  Box,
+  Button,
+  SvgIcon,
+  TextField,
+  Typography,
+  Badge,
+  Tooltip,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Workspace from "./Menus/Workspace";
+import Recent from "./Menus/Recent";
+import Started from "./Menus/Started";
+import Template from "./Menus/Template";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import Profile from "./Menus/Profile";
 
 function AppBar() {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: "primary.light",
+        backgroundColor: (theme) => theme.colorSchemes,
         width: "100%",
         height: (theme) => theme.projectCustom.appBarHeight,
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between ",
       }}
     >
-      {" "}
-      <DarkLightMode />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <MenuIcon />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 0.5,
+          }}
+        >
+          <SvgIcon component={TrelloIcon} inheritViewBox fontSize="small" />
+          <Typography
+            variant="span"
+            sx={{ fontSize: "1.2rem", fontWeight: "bold" }}
+          >
+            Trello
+          </Typography>
+        </Box>
+        <Workspace />
+        <Recent />
+        <Started />
+        <Template />
+        <Button variant="outlined">Create</Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <TextField
+          id="outlined-search"
+          label="Search field"
+          type="search"
+          size="small"
+        />
+        <DarkLightMode />
+        <Tooltip title="Notification">
+          <Badge color="secondary" variant="dot" sx={{cursor:"pointer"}}>
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title="Question?" sx={{cursor:"pointer"}}>
+           <QuestionMarkIcon/>
+        </Tooltip>
+        <Profile/>
+        
+      </Box>
     </Box>
   );
 }
